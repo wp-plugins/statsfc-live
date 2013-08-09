3d7ea451793eda78301ea627ac451c2525126041
willjw <willjw@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-08-09 16:44:47 +0000

    Removed unnecessary cURL option.
    git-svn-id: https://plugins.svn.wordpress.org/statsfc-live/trunk@754145 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- statsfc-live.php
+++ statsfc-live.php
@@ -3,7 +3,7 @@
 Plugin Name: StatsFC Live
 Plugin URI: https://statsfc.com/docs/wordpress
 Description: StatsFC Live
-Version: 1.2.1
+Version: 1.2.2
 Author: Will Woodward
 Author URI: http://willjw.co.uk
 License: GPL2
@@ -303,7 +303,6 @@ class StatsFC_Live extends WP_Widget {
 
 			curl_setopt_array($ch, array(
 				CURLOPT_AUTOREFERER		=> true,
-				CURLOPT_FOLLOWLOCATION	=> true,
 				CURLOPT_HEADER			=> false,
 				CURLOPT_RETURNTRANSFER	=> true,
 				CURLOPT_TIMEOUT			=> 5,
